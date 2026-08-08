3508cae095f5c9ede5a8602cb3e4bba738aacd32
anonimnianonist <vanjamba0211@gmail.com>
2026-08-09 01:05:18 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,4 @@
-queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/vfx-qudsa/newauto/refs/heads/main/.lua'))()")
 
-queue_on_teleport("_G.AutofarmEnabled = true; loadstring(game:HttpGet('https://raw.githubusercontent.com/vfx-qudsa/newguiauto/refs/heads/main/.lua'))()")
 
 _G.AutofarmEnabled = true
 
